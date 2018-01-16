ebd75c50c6b667cd31524380514bed0ebfc54b8f
Jeannot <contrevent@gmail.com>
2018-01-16 10:52:51 +0100

    Create trade quick validation

--- app/assets/javascripts/create_trade.js
+++ app/assets/javascripts/create_trade.js
@@ -5,31 +5,49 @@ $(function () {
         return this.length !== 0;
     };
 
-
-
     var inputCount = $('#ct-new-count');
     if (inputCount.exists()) {
         var selectSymbol = $('#ct-select-symbol');
         var inputStart = $('#ct-start-usd');
         var inputStop = $('#ct-stop-usd');
         var inputRisk = $('#ct-risk');
-
-
+        var createButton = $('#ct-create');
 
         function evalRisk() {
             try {
                 var count = parseFloat(inputCount.val());
                 var start = parseFloat(inputStart.val());
                 var stop = parseFloat(inputStop.val());
-                if (isNaN(count) || isNaN(start) || isNaN(stop)) {
+                var valid = true;
+                if (isNaN(count)) {
+                    valid = false;
+                    inputCount.addClass('is-invalid');
+                } else {
+                    inputCount.removeClass('is-invalid');
+                }
+                if (isNaN(start)) {
+                    valid = false;
+                    inputStart.addClass('is-invalid');
+                } else {
+                    inputStart.removeClass('is-invalid');
+                }
+                if (isNaN(stop)) {
+                    valid = false;
+                    inputStop.addClass('is-invalid');
+                } else {
+                    inputStop.removeClass('is-invalid');
+                }
+                if (!valid) {
                     inputRisk.val("n.a.");
+                    createButton.attr('disabled', 'disabled');
                 } else {
                     inputRisk.val((start - stop) * count);
+                    createButton.removeAttr('disabled');
                 }
             } catch (e) {
                 inputRisk.val("n.a.");
+                createButton.attr('disabled', 'disabled');
             }
-
         }
 
         function loadTicker() {
