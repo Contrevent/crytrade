ee053674117cbdee72e461b3b212687c4ea4ff7f
Jeannot <contrevent@gmail.com>
2018-01-17 21:37:18 +0100

    polishing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,16 +24,15 @@ class ApplicationController < ActionController::Base
          get_value: lambda {|currency| present((currency.volume_usd_24h / 1000000).round)}},
         {name: 'market_cap_usd', align: 'center', label: 'Market Cap (M$)',
          get_value: lambda {|currency| present((currency.market_cap_usd / 1000000).round)}},
-        {name: 'percent_change_1h', label: '1h &Delta; (%)', align: 'center',
-         get_value: lambda {|currency| arrow(currency.percent_change_1h)},
-         kind: lambda {|currency| kind.call(currency.percent_change_1h)}
+        {name: 'percent_change_1h', label: '1h &Delta; (%)', deco: true,
+         get_value: lambda {|currency| currency.percent_change_1h}
         },
-        {name: 'percent_change_24h', label: '24h &Delta; (%)', align: 'center',
-         get_value: lambda {|currency| arrow(currency.percent_change_24h)},
-         kind: lambda {|currency| kind.call(currency.percent_change_24h)}},
-        {name: 'percent_change_7d', label: '7d &Delta; (%)', align: 'center',
-         get_value: lambda {|currency| arrow(currency.percent_change_7d)},
-         kind: lambda {|currency| kind.call(currency.percent_change_7d)}},
+        {name: 'percent_change_24h', label: '24h &Delta; (%)', deco: true,
+         get_value: lambda {|currency| currency.percent_change_24h}
+        },
+        {name: 'percent_change_7d', label: '7d &Delta; (%)', deco: true,
+         get_value: lambda {|currency| currency.percent_change_7d}
+        },
         {name: 'cmc_link', label: 'Cmc', align: 'center',
          link: lambda {|currency| "https://coinmarketcap.com/currencies/#{currency.currency_id}/"}}
 
@@ -67,12 +66,4 @@ class ApplicationController < ActionController::Base
     helper.number_with_delimiter(value, precision: precision, delimiter: ' ')
   end
 
-  def arrow(value)
-    code = if value == 0 then
-             "="
-           else
-             value > 0 ? "&#9650;" : "&#9660;"
-           end
-    "#{value} #{code}".html_safe
-  end
 end
