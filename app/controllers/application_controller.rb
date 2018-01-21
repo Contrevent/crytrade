084b67165f70e1130f5dc80a42506e65c3a92117
Jeannot <contrevent@gmail.com>
2018-01-21 10:19:08 +0100

    Coins col sorting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,34 +3,43 @@ class ApplicationController < ActionController::Base
   include TickerConcern
 
   def index
-    @currencies = TickerConcern::last_ticker.order('volume_usd_24h DESC').limit(100)
+    if params.key? (:col) and params.key? (:dir)
+      order_name = params[:col]
+      order_direction = params[:dir] == 'asc' ? 'asc' : 'desc'
+    else
+      order_name = 'volume_usd_24h'
+      order_direction = 'desc'
+    end
 
-    @columns = [
-        {name: 'symbol', align: 'left'},
-        {name: 'percent_change_1h', label: '1h &Delta; (%)', deco: true,
+    @currencies = TickerConcern::last_ticker.where('volume_usd_24h > 10000000').order("#{order_name} #{order_direction}").limit(100)
+
+    col_defs = [
+        {name: 'symbol', allow_order: true},
+        {name: 'percent_change_1h', allow_order: true, label: '1h &Delta; (%)', deco: true,
          get_value: lambda {|currency| currency.percent_change_1h}
         },
-        {name: 'percent_change_24h', label: '24h &Delta; (%)', deco: true,
+        {name: 'percent_change_24h', allow_order: true, label: '24h &Delta; (%)', deco: true,
          get_value: lambda {|currency| currency.percent_change_24h}
         },
-        {name: 'percent_change_7d', label: '7d &Delta; (%)', deco: true,
+        {name: 'percent_change_7d', allow_order: true, label: '7d &Delta; (%)', deco: true,
          get_value: lambda {|currency| currency.percent_change_7d}
         },
-        {name: 'currency_name', align: 'left', label: 'Name'},
-        {name: 'price_usd', align: 'center', label: 'Price ($)',
+        {name: 'currency_name', allow_order: true, label: 'Name'},
+        {name: 'price_usd', allow_order: true, label: 'Price ($)',
          get_value: lambda {|currency| present(currency.price_usd, 3)}},
-        {name: 'price_btc', align: 'center', label: 'Price (BTC)'},
-        {name: 'volume_usd_24h', align: 'center', label: '24h Volume (M$)',
+        {name: 'price_btc', allow_order: true, label: 'Price (BTC)'},
+        {name: 'volume_usd_24h', allow_order: true, label: '24h Vol. (M$)',
          get_value: lambda {|currency| present((currency.volume_usd_24h / 1000000).round)}},
-        {name: 'market_cap_usd', align: 'center', label: 'Market Cap (M$)',
+        {name: 'market_cap_usd', allow_order: true, label: 'Market Cap (M$)',
          get_value: lambda {|currency| currency.market_cap_usd != nil ? present((currency.market_cap_usd / 1000000).round) : 'n.a.'}},
-        {name: 'rank', align: 'center'},
-        {name: 'cmc_link', label: 'Cmc', align: 'center',
+        {name: 'rank', allow_order: true},
+        {name: 'cmc_link', allow_order: false, label: 'Cmc',
          link: lambda {|currency| "https://coinmarketcap.com/currencies/#{currency.currency_id}/"}}
 
     ]
 
 
+    @columns = col_defs.map {|col_def| decorate_order(col_def, order_name, order_direction)}
     @last_update = TickerConcern::last_ticker_update
   end
 
@@ -50,6 +59,13 @@ class ApplicationController < ActionController::Base
 
 
   private
+
+  def decorate_order(col_def, order_name, order_direction)
+    if col_def[:name] == order_name
+      return col_def.merge({direction: order_direction})
+    end
+    col_def
+  end
 
   def helper
     @helper ||= Class.new do
